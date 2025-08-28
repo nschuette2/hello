5b9fa1adb35756de8ba269a6e029b8dfd3bc6c81
nschuette2 <nschuette2@huskers.unl.edu>
2025-08-28 11:32:08 -0500

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,22 @@
+/**
+ * Author: Noah Schuette
+ *
+ * Date: 2025/09/25
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello, I'm Noah!\n");
+  printf("Computer Engineering");
+  return 0;
+}
+
+
+
+
+
